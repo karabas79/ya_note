d2872598e0e0031de0a58ea46e4158337a1e32fc
Роман Кораблев <korabl79@mail.ru>
2025-03-01 18:26:55 +0300

    test_content PASSED unittest

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -8,25 +8,54 @@ from notes.models import Note
 User = get_user_model()
 
 
-class TestDetailPage(TestCase):
+class TestNotesList(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.news = Note.objects.create(
-            title='Тестовая заметка', text='Просто текст.'
-        )
-        # Сохраняем в переменную адрес страницы с новостью:
-        cls.detail_url = reverse('notes:detail', args=(cls.note.id,))
         cls.author = User.objects.create(username='Автор')
+        cls.not_author = User.objects.create(username='Не автор')
+        cls.note = Note.objects.create(
+            title='Тестовая заметка',
+            text='Просто текст.',
+            author=cls.author
+        )
 
-    def test_anonymous_client_has_no_form(self):
-        response = self.client.get(self.detail_url)
-        self.assertNotIn('form', response.context)
-
-    def test_authorized_client_has_form(self):
-        # Авторизуем клиент при помощи ранее созданного пользователя.
+    def test_notes_list_for_author(self):
         self.client.force_login(self.author)
-        response = self.client.get(self.detail_url)
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        object_list = response.context['object_list']
+        self.assertIn(self.note, object_list)
+
+    def test_notes_list_for_not_author(self):
+        self.client.force_login(self.not_author)
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        object_list = response.context['object_list']
+        self.assertNotIn(self.note, object_list)
+
+
+class TestPagesContainsForm(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор')
+        cls.note = Note.objects.create(
+            title='Тестовая заметка',
+            text='Просто текст.',
+            author=cls.author
+        )
+
+    def test_add_page_contains_form(self):
+        self.client.force_login(self.author)
+        url = reverse('notes:add')
+        response = self.client.get(url)
         self.assertIn('form', response.context)
-        # Проверим, что объект формы соответствует нужному классу формы.
+        self.assertIsInstance(response.context['form'], NoteForm)
+
+    def test_edit_page_contains_form(self):
+        self.client.force_login(self.author)
+        url = reverse('notes:edit', args=[self.note.slug])
+        response = self.client.get(url)
+        self.assertIn('form', response.context)
         self.assertIsInstance(response.context['form'], NoteForm)
